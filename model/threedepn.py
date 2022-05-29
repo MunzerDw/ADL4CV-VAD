0bc669977fa7d4aec37e5bcd3b5ddadcc7b07e2a
MunzerDw <munzerdwedari@gmail.com>
2022-05-29 15:00:50 +0200

    clean, comment

--- model/threedepn.py
+++ model/threedepn.py
@@ -1,91 +1,11 @@
 import torch
 import torch.nn as nn
 
-
-class ThreeDEPN(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.num_features = 32
-
-        # TODO: 4 Encoder layers
-        self.encoder1 = nn.Sequential(
-            nn.Conv3d(in_channels=2, out_channels=self.num_features, kernel_size=4, stride=2,padding=1),
-            nn.LeakyReLU(0.2)
-        )
-        self.encoder2 = nn.Sequential(
-            nn.Conv3d(in_channels=self.num_features, out_channels=self.num_features * 2, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(self.num_features * 2),
-            nn.LeakyReLU(0.2)
-        )
-        self.encoder3 = nn.Sequential(
-            nn.Conv3d(in_channels=self.num_features * 2, out_channels=self.num_features * 4, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(self.num_features * 4),
-            nn.LeakyReLU(0.2)
-        )
-        self.encoder4 = nn.Sequential(
-            nn.Conv3d(in_channels=self.num_features * 4, out_channels=self.num_features * 8, kernel_size=4, stride=1,padding=0),
-            nn.BatchNorm3d(self.num_features * 8),
-            nn.LeakyReLU(0.2)
-        )
-
-        # TODO: 2 Bottleneck layers
-
-            # TODO: partial predictor linear layers as per the paper
-        self.bottleneck = nn.Sequential(
-                nn.Linear(self.num_features*8, self.num_features*8),
-                nn.ReLU(),
-                nn.Linear(self.num_features * 8, self.num_features * 8),
-                nn.ReLU()
-            )
-
-        # TODO: 4 Decoder layers
-        self.decoder1 = nn.Sequential(
-            nn.ConvTranspose3d(in_channels=self.num_features*16, out_channels=self.num_features*4, kernel_size=4, stride=1, padding=0),
-            nn.BatchNorm3d(self.num_features*4),
-            nn.ReLU()
-        )
-        self.decoder2 = nn.Sequential(
-            nn.ConvTranspose3d(in_channels=self.num_features * 8, out_channels=self.num_features * 2, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(self.num_features * 2),
-            nn.ReLU()
-        )
-        self.decoder3 = nn.Sequential(
-            nn.ConvTranspose3d(in_channels=self.num_features * 4, out_channels=self.num_features, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(self.num_features ),
-            nn.ReLU()
-        )
-        self.decoder4 = nn.Sequential(
-            nn.ConvTranspose3d(in_channels=self.num_features * 2, out_channels=1, kernel_size=4, stride=2, padding=1)
-        )
-    def forward(self, x):
-        b = x.shape[0]
-        # Encode
-        # TODO: Pass x though encoder while keeping the intermediate outputs for the skip connections
-        # Reshape and apply bottleneck layers
-        x_e1 = self.encoder1(x)
-        x_e2 = self.encoder2(x_e1)
-        x_e3 = self.encoder3(x_e2)
-        x_e4 = self.encoder4(x_e3)
-        x = x_e4.view(b, -1)
-        x = self.bottleneck(x)
-        x = x.view(x.shape[0], x.shape[1], 1, 1, 1)
-        # Decode
-        # TODO: Pass x through the decoder, applying the skip connections in the process
-        x_d1 = self.decoder1(torch.cat([x,x_e4], dim=1))
-        x_d2 = self.decoder2(torch.cat([x_d1, x_e3], dim=1))
-        x_d3 = self.decoder3(torch.cat([x_d2, x_e2], dim=1))
-        x = self.decoder4(torch.cat([x_d3, x_e1], dim=1))
-        x = torch.squeeze(x, dim=1)
-        # TODO: Log scaling
-        x = torch.log(torch.abs(x)+1)
-        return x
 class ThreeDEPNDecoder(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.num_features = 32
-            # TODO: partial predictor linear layers as per the paper
         self.bottleneck = nn.Sequential(
                 nn.Linear(self.num_features*8, self.num_features*8),
                 nn.ReLU(),
@@ -115,18 +35,16 @@ class ThreeDEPNDecoder(nn.Module):
             nn.ConvTranspose3d(in_channels=self.num_features * 2, out_channels=1, kernel_size=4, stride=2, padding=1)
         )
     def forward(self, x):
-        # Encode
-
         # Reshape and apply bottleneck layers
-
         x = self.bottleneck(x)
         x = x.view(x.shape[0], x.shape[1], 1, 1, 1)
-        # Decode
 
+        # Decode
         x_d1 = self.decoder1(x)
         x_d2 = self.decoder2(x_d1)
         x_d3 = self.decoder3(x_d2)
         x = self.decoder4(x_d3)
         x = torch.squeeze(x, dim=1)
         x = torch.log(torch.abs(x)+1)
+        
         return x
---
+++ training/train.py
@@ -0,0 +1,151 @@
+from pathlib import Path
+
+import numpy as np
+import torch
+import torch.distributions as dist
+from model.threedepn import ThreeDEPNDecoder
+from data.shapenet import ShapeNet
+
+
+def train(model, train_dataloader, latent_vectors, latent_log_var, device, config):
+    # Define loss
+    reconstruction_loss_criterion = torch.nn.SmoothL1Loss()
+    reconstruction_loss_criterion.to(device)
+
+    # Define Optimizers
+    if config['vad_free']:
+        optimizer = torch.optim.Adam([
+            {
+                'params': model.parameters(),
+                'lr': config['learning_rate_model']
+            },
+            {
+                'params': latent_vectors,
+                'lr': config['learning_rate_code']
+            },
+            {
+                'params': latent_log_var,
+                'lr': config['learning_rate_log_var']
+            }
+        ])
+    else:
+        optimizer = torch.optim.Adam([
+            {
+                'params': model.parameters(),
+                'lr': config['learning_rate_model']
+            },
+            {
+                'params': latent_vectors,
+                'lr': config['learning_rate_code']
+            }
+        ])
+
+    # Here, we follow the original implementation to also use a learning rate scheduler -- it simply reduces the learning rate to half every 20 epochs
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+
+    model.train()
+
+    # Keep track of running average of train loss for printing
+    best_loss = float('inf')
+    train_loss_running = 0.
+    reconstruction_loss_running = 0.
+    kl_loss_running = 0.
+
+    for epoch in range(config['max_epochs']):
+        for batch_idx, batch in enumerate(train_dataloader):
+            # Move batch to device, set optimizer gradients to zero, perform forward pass
+            ShapeNet.move_batch_to_device(batch, device)
+            optimizer.zero_grad()
+
+            # Create distribution from latent codes and laten variances (each sample has its own distribution)
+            Dist = dist.Normal(latent_vectors[batch['index']], torch.exp(latent_log_var[batch['index']]))
+
+            # Sample from each distribution
+            x_vad = Dist.rsample()
+
+            # Forward pass
+            reconstruction = model(x_vad)
+            
+            # Targets
+            target = batch['target_df']
+            q_z = dist.Normal(0, 1)
+
+            # Compute loss, Compute gradients, Update network parameters
+            reconstruction_loss = reconstruction_loss_criterion(reconstruction, target)
+            kl_loss = torch.mean(dist.kl_divergence(Dist, q_z))
+            loss = reconstruction_loss + config['kl_weight'] * kl_loss
+
+            # Compute gradients
+            loss.backward()
+
+            # Update network parameters
+            optimizer.step()
+
+            # Logging
+            train_loss_running += loss.item()
+            reconstruction_loss_running += reconstruction_loss.item()
+            kl_loss_running += kl_loss.item()
+            iteration = epoch * len(train_dataloader) + batch_idx
+
+            if iteration % config['print_every_n'] == (config['print_every_n'] - 1):
+                train_loss = train_loss_running / config["print_every_n"]
+                print(f'[{epoch:03d}/{batch_idx:05d}] train_loss: {train_loss_running / config["print_every_n"]:.6f}'
+                      f' kl_loss: {kl_loss_running/ config["print_every_n"]:.6f} normal_loss: {reconstruction_loss_running/config["print_every_n"]:.6f}')
+                train_loss_running = 0.
+                reconstruction_loss_running = 0.
+                kl_loss_running = 0.
+
+                # Save model checkpoint
+                if train_loss < best_loss:
+                    torch.save(model.state_dict(), f'runs/{config["experiment_name"]}/model_best.ckpt')
+                    torch.save(latent_vectors, f'runs/{config["experiment_name"]}/latent_best.pt')
+                    torch.save(latent_log_var, f'runs/{config["experiment_name"]}/log_var_best.pt')
+                    best_loss = train_loss
+                    
+        # Update scheduler          
+        scheduler.step()
+
+
+def main(config):
+    # Declare device
+    device = torch.device('cpu')
+    if torch.cuda.is_available() and config['device'].startswith('cuda'):
+        device = torch.device(config['device'])
+        print('Using device:', config['device'])
+    else:
+        print('Using CPU')
+
+    # Create Dataloaders
+    train_dataset = ShapeNet('train' if not config['is_overfit'] else 'overfit')
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=config['batch_size'], 
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True,
+        # worker_init_fn=train_dataset.worker_init_fn  TODO: Uncomment this line if you are using shapenet_zip on Google Colab
+    )
+
+    # Instantiate model
+    model = ThreeDEPNDecoder()
+
+    # Initialize latent codes and latent variance
+    latent_vectors = torch.normal(mean=0, std=1, size=(len(train_dataset), config['latent_code_length']), device=device)
+    latent_vectors.requires_grad = True
+    latent_log_var = torch.zeros(len(train_dataset), config['latent_code_length'], device=device)
+    latent_log_var.requires_grad = config['vad_free']
+
+    # Load model if resuming from checkpoint
+    if config['resume_ckpt'] is not None:
+        model.load_state_dict(torch.load(f"runs/{config['resume_ckpt']}/model_best.ckpt", map_location='cpu'))
+        latent_vectors = torch.load(f"runs/{config['resume_ckpt']}/latent_best.pt", map_location = 'cpu')
+        latent_log_var = torch.load(f"runs/{config['resume_ckpt']}/log_var_best.pt", map_location = 'cpu')
+
+    # Move model to specified device
+    model.to(device)
+
+    # Create folder for saving checkpoints
+    Path(f'runs/{config["experiment_name"]}').mkdir(exist_ok=True, parents=True)
+
+    # Start training
+    train(model, train_dataloader, latent_vectors, latent_log_var, device, config)
