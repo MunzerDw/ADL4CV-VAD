2b0c858a36632a2111bd875d74682345b3cd575e
Munzer <munzerdwedari@gmail.com>
2022-06-25 15:37:17 +0200

    Auto stash before merge of "master" and "origin/master"

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -3,23 +3,23 @@ from model.threedepn import ThreeDEPNDecoder
 import trimesh as tm
 from skimage.measure import marching_cubes
 import numpy as np
-#import pyvista as pv
 from data.shapenet import ShapeNet
 
-def generate_samples(experiment, n):
+def generate_samples(experiment, n, device=None):
     samples = []
     # Load model
     model = ThreeDEPNDecoder()
-    model.load_state_dict(torch.load(f"runs/{experiment}/model_best.ckpt", map_location='cpu'))
+    model.load_state_dict(torch.load(f"runs/{experiment}/model_best.ckpt", map_location=device))
+    model.to(device)
     # Sample
-    for i in range(n):
+    for _ in range(n):
         # Dist = dist.Normal(torch.zeros(256), torch.ones(256))
-        x_vad = torch.randn(256).unsqueeze(0)
+        x_vad = torch.randn(256, device=device).unsqueeze(0)
         # Forward pass
         output_meshes_int = model(x_vad)
         # add to samples
         samples.append(output_meshes_int)
-    samples = torch.stack(samples)
+    samples = torch.stack(samples).to(device)
     return samples
 
 def convert_df_to_point_cloud(sample, count=5000):
@@ -34,7 +34,7 @@ def convert_df_to_point_cloud(sample, count=5000):
     point_cloud = tm.sample.sample_surface(mesh, count=count)
     return torch.from_numpy(point_cloud[0]).float()
 
-def convert_set_to_point_cloud(samples, count=10000):
+def convert_set_to_point_cloud(samples, count=10000, device=None):
     """
     input: 
         samples: tensor (batch_size, 32, 32, 32)
@@ -43,37 +43,40 @@ def convert_set_to_point_cloud(samples, count=10000):
     """
     point_clouds = []
     for sample in samples:
-        verts, faces, _, values = marching_cubes(sample.detach().numpy(), level=1)
+        verts, faces, _, values = marching_cubes(sample.cpu().detach().numpy(), level=1)
         mesh = tm.Trimesh(verts, faces, vertex_normals=values)
         point_cloud = tm.sample.sample_surface(mesh, count=count)
-        point_cloud = torch.from_numpy(point_cloud[0]).float()
+        point_cloud = torch.from_numpy(point_cloud[0]).float().to(device)
         point_clouds.append(point_cloud)
-    return torch.stack(point_clouds)
+    return torch.stack(point_clouds).to(device)
 
-def visualize_point_cloud(point_cloud):
-    # points is a 3D numpy array (n_points, 3) coordinates of a sphere
-    cloud = pv.PolyData(point_cloud)
-    cloud.plot()
+# def visualize_point_cloud(point_cloud):
+#     # points is a 3D numpy array (n_points, 3) coordinates of a sphere
+#     cloud = pv.PolyData(point_cloud)
+#     cloud.plot()
 
 # https://stackoverflow.com/questions/47060685/chamfer-distance-between-two-point-clouds-in-tensorflow
 
-def array2samples_distance(array1, array2):
+def _point_clouds_min_distance(point_cloud1, point_cloud2):
     """
     arguments: 
-        array1: the array, size: (num_point, num_feature)
-        array2: the samples, size: (num_point, num_feature)
+        point_cloud1: tensor (num_point, num_feature)
+        point_cloud2: tensor (num_point, num_feature)
     returns:
         distances: each entry is the distance from a sample to array1 
     """
-    num_point, num_features = array1.shape
-    expanded_array1 = torch.tile(array1, (num_point, 1))
+    num_point, num_features = point_cloud1.shape
+    expanded_array1 = torch.tile(point_cloud1, (num_point, 1)) # num_points * num_points, num_feature
     expanded_array2 = torch.reshape(
-            torch.tile(torch.unsqueeze(array2, 1), 
-                    (1, num_point, 1)),
-            (-1, num_features))
-    distances = torch.linalg.norm(expanded_array1-expanded_array2, axis=1)
-    distances = torch.reshape(distances, (num_point, num_point))
-    distances = torch.min(distances, axis=1)
+        torch.tile(
+            torch.unsqueeze(point_cloud2, 1), 
+            (1, num_point, 1)
+        ),
+        (-1, num_features)
+    ) # num_points * num_points, num_feature
+    distances = torch.linalg.norm(expanded_array1-expanded_array2, axis=1) # num_points * num_points
+    distances = torch.reshape(distances, (num_point, num_point)) # num_points, num_points
+    distances = torch.min(distances, axis=1) # num_points
     distances = torch.mean(distances.values)
     return distances
 
@@ -85,12 +88,12 @@ def chamfer_distance(point_cloud1, point_cloud2):
     output: float
     """
     dist = 0
-    av_dist1 = array2samples_distance(point_cloud1, point_cloud2)
-    av_dist2 = array2samples_distance(point_cloud2, point_cloud1)
-    dist = dist + av_dist1+av_dist2
+    av_dist1 = _point_clouds_min_distance(point_cloud1, point_cloud2)
+    av_dist2 = _point_clouds_min_distance(point_cloud2, point_cloud1)
+    dist = dist + av_dist1 + av_dist2
     return dist
 
-def mmd(set1, set2):
+def _mmd(set1, set2):
     """
     input: 
         set1: tensor (num_samples1, num_points, 3)
@@ -103,13 +106,28 @@ def mmd(set1, set2):
     for i, sample in enumerate(set1):
         sample_distances = []
         for sample2 in set2:
-            cf_distance = chamfer_distance_numpy(sample, sample2)
+            cf_distance = chamfer_distance(sample, sample2)
             sample_distances.append(cf_distance)
         mmd = sum(sample_distances) / len(sample_distances)
         distances.append(mmd)
         print(f"{i + 1}: {mmd}")
     return torch.stack(distances)
 
+def MMD(experiment, split, filter_class, n_samples, device=None):
+    # generate n new samples
+    samples = generate_samples(experiment, n_samples, device)
+    samples = samples.squeeze(1)
+    val = []
+    val_dataset = ShapeNet(split, filter_class=filter_class)
+    for data_dict in val_dataset:
+        target_df = torch.from_numpy(data_dict['target_df']).float().to(device)
+        val.append(target_df)
+    val = torch.stack(val).to(device)
+    # convert sets to pointclouds
+    samples_point_clouds = convert_set_to_point_cloud(samples, device=device)
+    val_point_clouds = convert_set_to_point_cloud(val, device=device)
+    mmd_value = _mmd(samples_point_clouds, val_point_clouds)
+    return mmd_value.mean(), mmd_value
 
 def IOU(experiment, split, filter_class, device):
     model = ThreeDEPNDecoder()
@@ -137,3 +155,4 @@ def IOU(experiment, split, filter_class, device):
         sum1 = sum1 + (torch.sum(i) / torch.sum(u)).item()
 
     return sum1 / len(dataset)
+    
